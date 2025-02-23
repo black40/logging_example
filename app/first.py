58c94ea9366f5308bc8f8036ae54eee96f053959
black40 <black40@i.ua>
2025-02-23 17:29:10 +0100

    add logger in first and second

--- app/first.py
+++ app/first.py
@@ -1,8 +1,12 @@
 import logging
 
 
+logger = logging.getLogger(__name__)
+
+
 class First:
     def __init__(self):
+        logger.info('init class First')
         self.current_number = 0
 
     def increment_number(self):
--- app/second.py
+++ app/second.py
@@ -1,8 +1,12 @@
 import logging
 
 
+logger = logging.getLogger(__name__)
+
+
 class Second:
     def __init__(self):
+        logger.info('init class Second')
         self.enabled = False
 
     def enable_system(self):
